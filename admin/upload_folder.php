070ef28c97caf79f875b40396000268d7b312244
iambhavesh09 <bhaveshjhawar09@gmail.com>
2024-12-18 17:39:57 +0530

    Both admin pages , portfolio changes

--- admin/upload_folder.php
+++ admin/upload_folder.php
@@ -39,9 +39,38 @@
     .btn-upload:hover {
       background-color: #0056b3;
     }
+    .navbar-custom {
+      background-color:rgb(164, 120, 218);
+    }
+    .navbar-custom .navbar-nav .nav-link {
+      color: #ffffff !important;
+    }
+    .navbar-custom .navbar-nav .nav-link:hover {
+      color: #d1d1d1 !important;
+    }
   </style>
 </head>
 <body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-custom">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#">Admin Panel</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Upload Media</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="upload_folder.php">Upload Folder</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 <div class="admin-container">
     <h1>Upload Media Folder Link</h1>
